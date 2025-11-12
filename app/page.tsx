51a8e33f93ec8098650946b939c753afc15a6533
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2025-11-12 13:27:34 +0100

    add repo

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from '@/components/theme-toggle';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-sans">
+      <h1>Init project</h1>
+      <ModeToggle />
+    </div>
+  );
+}
